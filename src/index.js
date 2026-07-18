b6e1d5e855acd6dc46ca4a35ff3397c0dcef8bbf
jassi329 <jassisk329@gmail.com>
2026-07-18 14:03:34 +0530

    Fix CORS header to allow all origins

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const apiRoutes = require('./routes');
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL || 'https://flight-booking-system-frontend-rgvv.onrender.com');
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL || '*https://flight-booking-system-frontend-rgvv.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
